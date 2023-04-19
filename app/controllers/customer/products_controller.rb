763464c0d45029d526001df70eeee3fa51c7c4d4
shizuki-okuma <126657775+shizuki-okuma@users.noreply.github.com>
2023-04-19 04:05:57 +0000

    add products/index show

---
+++ app/controllers/customer/products_controller.rb
@@ -0,0 +1,13 @@
+class Customer::ProductsController < ApplicationController
+  def index
+    @products=Product.all
+  end
+
+  def show
+    @product=Product.find(params[:id])
+  end
+  
+  def new
+  end
+  
+end
