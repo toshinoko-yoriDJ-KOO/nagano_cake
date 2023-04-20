aefff28c1adee006b70721b160bb97edf6a78a60
shizuki-okuma <126657775+shizuki-okuma@users.noreply.github.com>
2023-04-20 04:33:39 +0000

    コンフリクト修正

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -4,6 +4,10 @@ class Customer::ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+    
+  end
+private
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
   end
 end
