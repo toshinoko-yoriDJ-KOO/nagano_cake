3f16ba9e8955c14883befc241f431ad32aca11a5
Ai1023 <126910091+Ai1023@users.noreply.github.com>
2023-04-26 10:58:25 +0000

    微調整

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -19,7 +19,6 @@ class Customer::CustomersController < ApplicationController
         @customer = current_customer
         @customer.update(is_active: false)
         reset_session
-        flash[:notice] = "またのご利用をお待ちしております！"
         redirect_to root_path
     end
 
